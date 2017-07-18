dda604c95f0d8478d77ebc1696b8429068a8b8e9
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-07-17 17:09:24 -0700

    Plotting graph

---
+++ MatplotLib.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 17:03:03 2017
+
+@author: aakash.chotrani
+"""
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+
+
+plt.plot([5,6,7,8],[7,3,4,1])
+plt.show()
