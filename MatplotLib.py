96f375342df3bfd1294b720e43631202ef68b168
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-07-17 17:44:29 -0700

    labels and legends
    Making graph easy to understand

--- MatplotLib.py
+++ MatplotLib.py
@@ -7,7 +7,17 @@ Created on Mon Jul 17 17:03:03 2017
 
 import matplotlib.pyplot as plt
 
+x = [5,6,7,8]
+y = [7,3,4,1]
 
+x2 = [5,6,7,8]
+y2 = [10,14,12,5]
 
-plt.plot([5,6,7,8],[7,3,4,1])
+plt.plot(x,y, label = "first line")
+plt.plot(x2,y2, label = "second line")
+plt.xlabel("x label")
+plt.ylabel("y label")
+
+plt.title("Interesting Graph \ncheck it out")
+plt.legend()
 plt.show()
